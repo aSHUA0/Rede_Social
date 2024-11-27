6bdb03d89047bfda592fa70b0223b05247a347e4
Pedro Carneiro <pcar3012@gmail.com>
2024-11-27 15:38:45 -0300

    Update rede_social_insert.sql

--- rede_social_insert.sql
+++ rede_social_insert.sql
@@ -1,1 +1,6 @@
+INSERT INTO USUARIO VALUES 
+(NULL, 'João Silva', 'joao.silva@example.com', NULL, '1990-05-14'),
+(NULL, 'Maria Oliveira', 'maria.oliveira@example.com', NULL, '1988-11-22'),
+(NULL, 'Carlos Santos', 'carlos.santos@example.com', NULL, '1995-03-07'),
+(NULL, 'Ana Costa', 'ana.costa@example.com', NULL, '2000-09-18');
 
